d5ede12f82e13969440dfe7cbf7a8247c7ed9b81
Margaryta Tchouikevitch <margaryta@gmail.com>
2018-01-25 21:23:21 -0500

    mostly UI fixes

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,29 +1,33 @@
 module GamesHelper
   def get_user_name(user)
-    if user 
+    if user
       return user.email
     end
     return "Guest"
   end
 
   def get_hidden_tag_w_user_id
+    uid = nil
     if current_user
-      hidden_field_tag :user_id, current_user.id
-    else
-      hidden_field_tag :user_id, nil
+      uid = current_user.id
     end
+    hidden_field_tag :user_id, uid
   end
 
   def get_word_chars_display
-    content_tag :div, class: "row " do 
-      @game.word.each_char do |c|
-        lbl = "_"
-        if (c.to_s.match /[^a-zA-Z]/ ) || (@game.current_guesses.include? c.to_s.downcase)
-          lbl = c.to_s
-        end
-        concat (content_tag :div, lbl,  class: "col-sm-1 word-letter")
-      end
-    end
+    content_tag :table, class: "table-bordered page-content" do
+      content_tag :tbody do
+        content_tag :tr do
+          @game.word.each_char do |c|
+            lbl = "_"
+            if (c.to_s.match /[^a-zA-Z]/ ) || (@game.current_guesses.include? c.to_s.downcase)
+              lbl = c.to_s
+            end
+            concat (content_tag :td, lbl, class: "word-letter")
+          end # each char
+        end # tr
+      end # tbody
+    end # table
   end
 
   def get_hangman_image(num)
@@ -33,23 +37,23 @@ module GamesHelper
   end
 
   def get_keyboard
-    content_tag :div, class: "container-fluid" do 
+    content_tag :div, class: "container-fluid" do
       concat (get_keyboard_row([:Q, :W, :E, :R, :T, :Y, :U, :I, :O, :P]))
       concat (get_keyboard_row([:A, :S, :D, :F, :G, :H, :J, :K, :L]))
       concat (get_keyboard_row([:Z, :X, :C, :V, :B, :N, :M]))
     end
   end
 
-  def get_game_over_banner 
-    if @game.user_won != nil 
+  def get_game_over_banner
+    if @game.user_won != nil
       extra_class = "user-lost-banner"
       caption = "You lost! The word was \"#{@game.word}\"."
-      if @game.user_won == true 
+      if @game.user_won == true
         extra_class = "user-won-banner"
         caption = "You won!"
       end
 
-      content_tag :div, caption, class: "container-fluid game-over-banner #{extra_class}"
+      content_tag :div, caption, class: "container game-over-banner #{extra_class}"
     end
   end
 
@@ -65,17 +69,15 @@ module GamesHelper
 
   def get_button_cell(label)
     str = label.to_s
-    extra_class = nil 
+    extra_class = nil
     if @game.current_guesses.include? str.downcase
       extra_class = "disabled"
     end
 
     css_class = "btn btn-info #{extra_class}"
-    content_tag :div, class: "col-sm-1" do 
+    content_tag :div, class: "col-sm-1" do
       concat (button_to str, { action: "update", :guess => str}, {method: :patch, remote: true, class: css_class, disabled: (@game.current_guesses.include? str.downcase)})
     end
   end
-
-
 
 end
